97b13fe185cf4d827aa7436425bb04f2a888b468
YOUSSIFRAbie <116589902+YOUSSIFRAbie@users.noreply.github.com>
2023-03-05 15:01:32 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,18 @@
+let header = document.querySelector(".header");
+let toogle = document.querySelector(".fa-bars");
+let nav = document.querySelector(".links-h");
+let close = document.querySelector(".close");
+
+toogle.onclick = function () {
+  nav.classList.add("open");
+};
+
+close.onclick = function () {
+  this.parentElement.classList.remove("open");
+};
+
+document.onkeyup = function (e) {
+  if (e.key === "Escape") {
+    nav.classList.remove("open");
+  }
+};
